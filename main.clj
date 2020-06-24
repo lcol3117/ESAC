6a01a01f1c2113e3b24f3b844751d1f4485db01a
Landon Colaresi <landoncol3117@gmail.com>
2020-06-24 20:23:04 +0000

    density-transform, density, and avg seem good

--- main.clj
+++ main.clj
@@ -4,6 +4,12 @@
 (defn density
   [data r point]
   (do
-    (def dists (map (fn [x] (dist point x))))
-    (def within (filter (fn [x] (> r x)) dists))
+    (def dists
+      (map (fn [x] (dist point x)) data))
+    (def dists-within
+      (filter (fn [x] (> r x)) dists))
+    (* (/ r) avg dists-within)
   ))
+(defn avg
+  [given-list]
+  (* (/ (count given-list)) (reduce + given-list)))
