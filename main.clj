ef8611bc58453d3a9519313208cc66b8b05b9fd7
Landon Colaresi <landoncol3117@gmail.com>
2020-06-24 20:01:09 +0000

    fix indentation

--- main.clj
+++ main.clj
@@ -3,7 +3,6 @@
   (map (fn [x] (density data r x)) data))
 (defn density
   [data r point]
-  (->>
-    (map (fn [x] (dist point x)))
+  (->> (map (fn [x] (dist point x)))
     (filter (fn [x] (> r x)))
   ))
