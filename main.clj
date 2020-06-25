739c098ee00af7900ac83ee829dc6f04ded0753d
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-06-25 14:51:14 -0400

    finish density-transform

--- main.clj
+++ main.clj
@@ -1,6 +1,13 @@
-(defn density-transform
-  [data r]
-  (map (fn [x] (density data r x)) data))
+(defn dist
+    [a b]
+    (do
+        (def d-range (range (count a)))
+        (def d-items (map (fn [x] (Math/pow 2 (- (get b x) (get a x)))) d-range))
+        (def sum-d (reduce + d-items))
+        (Math/sqrt sum-d)))
+(defn avg
+  [given-list]
+  (* (/ (count given-list)) (reduce + given-list)))
 (defn density
   [data r point]
   (do
@@ -8,8 +15,7 @@
       (map (fn [x] (dist point x)) data))
     (def dists-within
       (filter (fn [x] (> r x)) dists))
-    (* (/ r) avg dists-within)
-  ))
-(defn avg
-  [given-list]
-  (* (/ (count given-list)) (reduce + given-list)))
+    (* (/ r) (avg dists-within))))
+(defn density-transform
+  [data r]
+  (map (fn [x] (density data r x)) data))
